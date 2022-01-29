df4a12083df295ed2a0cb1685526a440d3933580
uzaircode <nikuzairsc@gmail.com>
2022-01-29 23:21:19 +0800

    final commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
 
+using namespace std;
+
+class Stack {
+private :
+    char stack[5];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    int full() {
+        if (top == 4)
+            return 1;
+        else
+            return 0;
+    }
+
+
+    int empty() {
+        if (top == -1)
+            return 1;
+        else
+            return 0;
+    }
+
+    void push(char alpha) {
+        if (!full()) {
+            top++;
+            stack[top] = alpha;
+        } else
+            cout << "Stack is full.";
+    }
+
+    char pop() {
+        char alpha;
+        if (!empty()) {
+            alpha = stack[top];
+            top--;
+            return alpha;
+        } else
+            cout << "Stack is empty.";
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Stack st;
+    char alpha;
+
+    cout << "Key in five characters : ";
+    for (int i = 0; i < 5; i++) {
+        cin >> alpha;
+        st.push(alpha);
+    }
+
+    cout << "\nThe reverse order : ";
+    while(!st.empty())
+    {
+        cout << st.pop() << " ";
+    }
+
     return 0;
 }
